5910daa9fbec6cc90a04f0cd42e75a7476566ecb
AbdullaAsaad <aboody.asaad99@gmail.com>
2022-10-28 22:35:32 +0300

    simple blog with inertia

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Post;
+use Inertia\Inertia;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PostRequest;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::query()
+            ->when(
+                request('search'),
+                fn ($query) => $query->where('name', 'LIKE', '%' . request('search') . '%')
+            )
+            ->latest()
+            ->paginate(5);
+
+        return Inertia::render('Admin/Posts/Index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Admin/Posts/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostRequest $request)
+    {
+        Post::create($request->validated());
+
+        return redirect()
+            ->route('admin.posts.index')
+            ->with('message', 'Post Added');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return Inertia::render('Admin/Posts/Edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostRequest $request, Post $post)
+    {
+        $post->update($request->validated());
+
+        return redirect()
+            ->route('admin.posts.index')
+            ->with('message', 'Post Updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()
+            ->route('admin.posts.index')
+            ->with('message', 'Post Deleted');
+    }
+}
